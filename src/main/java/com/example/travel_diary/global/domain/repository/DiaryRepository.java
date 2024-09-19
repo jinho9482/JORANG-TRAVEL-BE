49be72a78691966d22560f2e623e61ec2fa31f16
jinho9482 <jinhojo9482@gmail.com>
2024-09-19 13:57:24 +0900

    [Add] api to search post

--- src/main/java/com/example/travel_diary/global/domain/repository/DiaryRepository.java
+++ src/main/java/com/example/travel_diary/global/domain/repository/DiaryRepository.java
@@ -3,12 +3,20 @@ package com.example.travel_diary.global.domain.repository;
 import com.example.travel_diary.global.domain.entity.Diary;
 import com.example.travel_diary.global.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
     List<Diary> findAllByPost_Id(Long postId);
     List<Diary> findAllByPost_User(User user);
 
+    @Query(value = "SELECT DISTINCT d.content FROM diaries d " +
+            "INNER JOIN posts p ON p.post_id = d.post_id " +
+            "INNER JOIN users u ON u.user_id = p.user_id " +
+            "WHERE p.user_id = :userId", nativeQuery = true)
+    List<String> findMyDiaryContents(@Param("userId") UUID userId);
 }
--- src/main/java/com/example/travel_diary/global/request/PostTempRequest.java
+++ src/main/java/com/example/travel_diary/global/request/PostTempRequest.java
@@ -4,12 +4,15 @@ import com.example.travel_diary.global.domain.entity.Post;
 import com.example.travel_diary.global.domain.entity.User;
 import com.example.travel_diary.global.domain.type.Scope;
 
+import java.time.LocalDateTime;
+
 public record PostTempRequest(
         String title
 ) {
     public Post toEntity(User user) {
         Post post = Post.builder()
                 .title(title)
+                .createdAt(LocalDateTime.now())
                 .user(user)
                 .build();
         return post;
